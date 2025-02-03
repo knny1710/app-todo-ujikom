254926998e9772f8a3eb9196e02c173c318430e8
knny1710 <2223613.sandi@smkn-2sbg.sch.id>
2025-02-03 13:41:31 +0700

    komentar/dokumentasi

--- database/seeders/TaskListSeeder.php
+++ database/seeders/TaskListSeeder.php
@@ -1,12 +1,16 @@
 <?php
 
 namespace Database\Seeders;
+// database seeder adalah namespace default yang digunakan untuk semua seeder di laravel
+// seeder adalah class yang dijalankan ketika database dijalankan
+// namespace di php adalah cara untuk mengelompokan class agar tidak terjadi konflik nama antara class dengan class lain
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\TaskList;
 
 class TaskListSeeder extends Seeder
+// class TaskListSeeder extends Seeder merupakan class yang dijalankan ketika database dijalankan 
 {
     /**
      * Run the database seeds.
@@ -23,6 +27,7 @@ class TaskListSeeder extends Seeder
             [
                 'name' => 'Tugas',
             ]
+            
         ];
 
         TaskList::insert($lists);
